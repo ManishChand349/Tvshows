b233065051fd69187addf9f30d959533e4816701
Manish Chand <manishchandr349@gmail.com>
2024-01-22 18:28:45 +0530

    adding

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -14,12 +14,12 @@ const ShowList = () => {
   }, []);
 
   return (
-    <div className="container mx-auto mt-8">
+    <div className="max-w-[80%] mx-auto  mt-8">
       <h1 className="text-4xl font-bold mb-4 text-center">TV Shows</h1>
       
-      <ul className="grid grid-cols-4 gap-2 text-lg text-gray-800 mb-10">
+      <ul className="grid grid-cols-1 gap-2 text-lg text-gray-800 mb-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
         {shows.map((show) => (
-          <li key={show.show.id} className="mb-4 ">
+          <li key={show.show.id} className="mb-4 mx-auto">
             <Link to={`/show/${show.show.id}`} className="h-56 w-full rounded-md object-cover">
             <img src={show.show.image && show.show.image.medium} alt="" className="object-cover sm:h-80 lg:h-96  rounded-lg" />
             </Link>
